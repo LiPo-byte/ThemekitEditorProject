97b977c9a396994f2f4969660b099d9551536023
LiPo-byte <tong.he@woohooart.com>
2026-08-17 15:06:09 +0800

    feat: 修改上传文件大小限制&优化battery 0

--- frontend/src/pages/editor-xyflow/widget/battery-layout_0.tsx
+++ frontend/src/pages/editor-xyflow/widget/battery-layout_0.tsx
@@ -119,6 +119,7 @@ export default function BatteryLayout_0(props: any) {
               style={{
                 ...getTextStyle(props.parentId, data.battery),
                 margin: `${data.battery.intCommonField}px 0`,
+                marginRight: `${data.battery.padding}px`,
                 color: activeBattery?.key !== 'battery_20' ? data.battery.textColor : '#ff0000',
               }}
             >
